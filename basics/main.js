8e6b880c3ed56ca81bbeada2b468d71c8692e813
SoulVersion <rijothomas120@gmail.com>
2019-05-05 15:44:49 +0530

    added basics folder

---
+++ basics/main.js
@@ -0,0 +1,10 @@
+let num = 200; //variable 1
+let ans = num%2; //variable 2
+
+//below is the conditional statement
+if (ans === 0){
+	document.write("number is even"); //javascript method to display content
+}
+else{
+	document.write("number is odd");
+}
---
+++ basics/project/main.js
@@ -0,0 +1,12 @@
+let num = 200; //variable 1
+let ans = num%2; //variable 2
+
+//below is the conditional statement
+if (ans === 0){
+	document.write("number is even"); //javascript method to display content
+        num = num + 10; //adding 10 to the num variable ie (num = 190)
+}
+else{
+	document.write("number is odd");
+        num = num - 10; //subtracting 10 from num variable ie (num = 210)
+}
